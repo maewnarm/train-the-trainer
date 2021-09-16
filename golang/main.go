db67582e432077d460703bab29b393a585d73f99
maewnarm-ubuntu <vorametz@gmail.com>
2021-09-16 16:41:51 +0700

    hw2-com

--- golang/main.go
+++ golang/main.go
@@ -2,34 +2,60 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"strconv"
-	"time"
 
 	"github.com/fatih/color"
 	//"test-go/shapes"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixMicro())
-	n := rand.Intn(100)
-	input := ""
-	maxTry := 5
-	fmt.Println("Generated random number")
-	for try := 0; try < maxTry; try += 1 {
-		fmt.Printf("Input guessed number (%v/%v):", try+1, maxTry)
-		fmt.Scanln(&input)
-		inp, err := strconv.Atoi(input)
-		if err != nil {
-			panic("convert error")
-		}
-		if inp < n {
-			color.Yellow("TOO LOW")
-		} else if inp > n {
-			color.Red("TOO HIGH")
-		} else {
-			color.Green("CORRECT !!")
-			return
-		}
+	arrApp := [6]string{
+		"1: FIBONACCI",
+		"2: FizzBuzz",
+		"3: Star inc-down-right",
+		"4: Star inc-down-left",
+		"5: Star diamond",
+		"6: Tally - count char in string"}
+	color.Green("choose application :")
+	var selApp, input1, input2 int
+	var inpTxt1, inpTxt2 string
+	fmt.Scanln(&selApp)
+	fmt.Println("you choose application : " + arrApp[selApp])
+	switch selApp {
+	case 1:
+		fmt.Print("input 1 = ")
+		fmt.Scanln(&input1)
+		fmt.Print("input 2 = ")
+		fmt.Scanln(&input2)
+		fibo(input1, input2)
+	case 2:
+		fmt.Print("input 1 = ")
+		fmt.Scanln(&input1)
+		fmt.Print("input 2 = ")
+		fmt.Scanln(&input2)
+		fizzbuzz(input1, input2)
+	case 3, 4, 5:
+		drawstar(selApp)
+	case 6:
+		fmt.Print("find text = ")
+		fmt.Scan(&inpTxt1)
+		fmt.Print("in text = ")
+		fmt.Scan(&inpTxt2)
+		tallyTxt(inpTxt1, inpTxt2)
 	}
+}
+
+func fibo(num1 int, num2 int) {
+
+}
+
+func fizzbuzz(num1 int, num2 int) {
+
+}
+
+func drawstar(mode int) {
+
+}
+
+func tallyTxt(findTxt string, fromTxt string) {
+
 }
